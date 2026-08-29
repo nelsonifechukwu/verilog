8dcfeb67fbe72b2baf6a5bc49471deeaec2bb648
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2026-08-29 14:52:13 +0100

    improved fsm testbench: check that states are changing based on inputs

--- fsm/fsm_tb.v
+++ fsm/fsm_tb.v
@@ -16,17 +16,28 @@ module fsm_tb;
 
     initial begin
         clk = 1'b0;
-        reset = 1'b1;
-        start = 1'b0;
-        stop = 1'b0;
+        reset = 1'b0;
+        start = 1'b1;
+        stop = 1'b1;
 
-        @(posedge clk); 
-        #1;
+        // @(posedge clk); 
+        // #1;
         
-        if (running)
-            $display("FAILED, reset is 1");
-        else
-            $display("Reset test passed. Running = %b", running);
+        // if (running)
+        //     $display("FAILED, reset is 1");
+        // else
+        //     $display("Reset test passed. Running = %b", running);
+
+          // Print at t = 0
+        $strobe("time=%0t ns, clk=%b, running=%b", $time, clk, running);
+
+        repeat (5) begin
+            #5;
+          // Print at t = 0
+            $strobe("time=%0t ns, clk=%b, running=%b",
+                $time, clk, running);
+        end
+        
         $finish;
     end
 endmodule
