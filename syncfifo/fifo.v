1d39ec49ba213a4dba1f7199ad06b3b9794b4037
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2026-08-29 15:28:10 +0100

    update comment

--- syncfifo/fifo.v
+++ syncfifo/fifo.v
@@ -3,6 +3,6 @@ module fifo();
     reg [7:0] mem [0:4];
     reg [1:0] w_ptr;
     reg [1:0] r_ptr;
-    reg [2:0] count; //count count must represent 0 ... 4 (5 values), so we need more bits as 2 bits only allow us to represent 4 values.
+    reg [2:0] count; //count must represent 0...4 (5 values), so we need more bits as 2 bits only allow us to represent 4 values.
 
 endmodule
