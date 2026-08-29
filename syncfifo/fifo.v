5ee80630a3ef10f86631fafd6566e2c7cf6d6f9c
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2026-08-29 15:50:10 +0100

    implement read from and write to fifo

--- syncfifo/fifo.v
+++ syncfifo/fifo.v
@@ -1,7 +1,8 @@
 module fifo(
-    input wire clk, reset;
-    input wire w_en, r_en;
-    input wire [7:0] w_data, r_data;
+    input wire clk, reset,
+    input wire w_en, r_en,
+    input wire [7:0] w_data,
+    output reg r_data,
     output wire empty, full
 );
 
@@ -13,4 +14,26 @@ module fifo(
     assign empty = (count == 0);
     assign full  = (count == 4);
 
+    always @(posedge clk) begin
+
+        if (reset) begin //cause multiple statements are being controlled
+            w_ptr  <= 0;
+            r_ptr  <= 0;
+            count   <= 0;
+            r_data <= 0;
+
+        end else begin
+            //write
+            if (w_en && !full) begin
+                mem[w_ptr] <= w_data;
+                w_ptr <= w_ptr + 1;
+            end
+
+            //read
+            if (r_en && !empty) begin
+                r_data <= mem[r_ptr];
+                r_ptr <= r_ptr + 1;
+            end 
+        end
+    end
 endmodule
