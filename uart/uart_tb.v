5df34be7d765747580afaee74c1c6dabb9457dfd
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2026-08-31 04:24:21 +0100

    corrected comment

--- uart/uart_tb.v
+++ uart/uart_tb.v
@@ -15,7 +15,7 @@ module uart_tb;
     );
 
     always #5 clk = ~clk;
-    always #5 tick = ~tick; //clk period = 1 UART bit perios
+    always #5 tick = ~tick; //clk period = 1 UART bit period
 
     initial begin
         data = 8'b11001101;
